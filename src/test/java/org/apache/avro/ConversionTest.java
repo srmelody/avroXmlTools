f080a48c8d81bbed3fb7d824f97a6274705a2e7a
Your Name <srmelody@gmail.com>
2011-12-13 07:33:17 -0500

    Ed-fi classes via xjc

--- src/test/java/org/apache/avro/ConversionTest.java
+++ src/test/java/org/apache/avro/ConversionTest.java
@@ -2,10 +2,16 @@ package org.apache.avro;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 
 import org.apache.avro.compiler.specific.SpecificCompiler;
+import org.apache.avro.reflect.ReflectData;
+import org.ed_fi._0100rfc.School;
+import org.ed_fi._0100rfc.Student;
+import org.ed_fi._0100rfc.StudentSchoolAssociation;
 import org.junit.Test;
 
 public class ConversionTest {
@@ -17,14 +23,36 @@ public class ConversionTest {
 
 		InputStream is = new FileInputStream(fileName);
 		XsdConverter c = new XsdConverter();
-		c.convert( is );
-		}
+		c.convert(is);
+	}
 
 	@Test
 	public void generate() throws IOException {
-		
-		// Template directory is configurable - record.vm in templates is what is being used.
-		
-		SpecificCompiler.compileSchema(new File("src/main/resources/avpr/AcademicWeek.avpr"), new File("src/main/resources/avro/generated/"));
+
+		// Template directory is configurable - record.vm in templates is what
+		// is being used.
+
+		SpecificCompiler.compileSchema(new File(
+				"src/main/resources/avpr/AcademicWeek.avpr"), new File(
+				"src/main/resources/avro/generated/"));
 	}
+
+	@Test
+	public void generateFromJaxb() throws Exception {
+
+		Class klass = Student.class;
+		writeToFile(ReflectData.get().getSchema(klass), "student_body.avpr");
+		writeToFile(ReflectData.get().getSchema(School.class), "school_body.avpr");
+		writeToFile(ReflectData.get().getSchema(StudentSchoolAssociation.class), "studentSchoolAssociation_body.avpr");
+
+	}
+
+	private void writeToFile(Schema schema, String filename ) throws Exception {
+		PrintWriter writer = new PrintWriter( "src/main/resources/avro/generated/" + filename );
+		String s = schema.toString();
+		writer.println( s );
+		writer.close();
+		
+	}
+
 }
